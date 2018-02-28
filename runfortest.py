9dcf582e490cabc10bff567a4889aa57ee3d1a3f
vishwajeet1132 <vishu1132@gmail.com>
2018-02-28 14:23:05 +0530

    new commit

--- runfortest.py
+++ runfortest.py
@@ -3,6 +3,7 @@ import requests
 def post():
     url = 'http://127.0.0.1:5000/post_location'
     data=["56.88+63.75+IN/45645+vijay nagar+tamil nadu","28.6333+77.2167+IN/110001+Cannught place+New Delhi"]
+    headers = {'Content-type': 'text/plain'}
     for i in data:
         response = requests.post(url, data=i,headers=headers)
         print(response.text)
@@ -10,6 +11,7 @@ def post():
 def get_postgres():
     url = 'http://127.0.0.1:5000/get_using_postgres'
     data=["28.6488+77.1726+5000","28.6333+77.2167+3000"]
+    headers = {'Content-type': 'text/plain'}
     for i in data:
         response = requests.post(url, data=i,headers=headers)
         print(response.text)
@@ -17,13 +19,20 @@ def get_postgres():
 def get_self():
     url = 'http://127.0.0.1:5000/get_using_self'
     data=["28.6488+77.1726+5000","28.6333+77.2167+3000"]
+    headers = {'Content-type': 'text/plain'}
     for i in data:
         response = requests.post(url, data=i,headers=headers)
         print(response.text)
 
 def get_cityname():
-    url = 'http://127.0.0.1:5000/get_using_postgres'
+    url = 'http://127.0.0.1:5000/get_city_name'
     data=["28.6488+77.1726","28.6333+77.2167"]
+    headers = {'Content-type': 'text/plain'}
     for i in data:
         response = requests.post(url, data=i,headers=headers)
         print(response.text)
+
+post()
+get_postgres()
+get_self()
+get_cityname()
